54eb7d1bca4c0eb986db14a5f10268cef1c318d2
dveloperY0115 <dreamy1534@gmail.com>
2021-07-10 22:34:03 +0900

    feat(main): Add executable models

--- chapter9_rnn/models.py
+++ chapter9_rnn/models.py
@@ -4,25 +4,40 @@ import torch.nn.functional as F
 
 
 class SimpleRNN(nn.Module):
+    def __init__(
+        self, num_layers, hidden_dim, num_vocab, embed_dim, num_classes, dropout_p=0.5, **kwargs
+    ):
+        """
+        Constructor of SimpleRNN
 
-    def __init__(self, num_layers=2, num_category=4):
+        Args:
+        - embed_args (tuple). A tuple containing arguments for nn.Embedding
+        - rnn_args (tuple). A tuple containing arguments for nn.RNN
+        """
 
-        super().__init__(SimpleRNN, self)
-        
+        super(SimpleRNN, self).__init__()
+
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+
+        # embedding layer
+        self.embed = nn.Embedding(num_vocab, embed_dim)
+
         # basic RNN
-        self.recurrent = nn.RNN(input_size=128, hidden_size=32,
-                            num_layers=num_layers, batch_first=True)
-        
+        self.recurrent = nn.RNN(embed_dim, hidden_dim, num_layers=self.num_layers, batch_first=True)
+
         # fully connected layers for texts
         self.fc_1 = nn.Linear(32, 16)
-        self.fc_2 = nn.Linear(16, num_category)
+        self.fc_2 = nn.Linear(16, num_classes)
 
-        self.fc_3 = nn.Linear(5, 32)
+        self.fc_3 = nn.Linear(kwargs["num_publisher"], 32)
         self.fc_4 = nn.Linear(32, 16)
-        self.fc_5 = nn.Linear(16, num_category)
+        self.fc_5 = nn.Linear(16, num_classes)
 
+        # drop-out layer
+        self.do = nn.Dropout(dropout_p)
 
-    def forward(x, z):
+    def forward(self, x, z):
         """
         Forward propagation.
 
@@ -30,38 +45,56 @@ class SimpleRNN(nn.Module):
         - x: Batch of texts.
         - z: Batch of publisher labels.
         """
-        x = self.recurrent(x)
+
+        x = self.embed(x)
+        h_0 = self._init_state(batch_size=x.size(0))
+        x, _ = self.recurrent(x, h_0)
+        x = x[:, -1, :]
         x = F.relu(self.fc_1(x))
         x = F.relu(self.fc_2(x))
 
-        z = F.relu(self.fc_3(z))
+        z = F.relu(self.fc_3(z.type(torch.float)))
         z = F.relu(self.fc_4(z))
         z = F.relu(self.fc_5(z))
 
-        out = F.softmax(x + z)
+        out = x + z
         return out
+
+    def _init_state(self, batch_size=1):
+        weight = next(self.parameters()).data
+        return weight.new(self.num_layers, batch_size, self.hidden_dim).zero_()
 
 
 class SimpleLSTM(nn.Module):
+    def __init__(
+        self, num_layers, hidden_dim, num_vocab, embed_dim, num_classes, dropout_p=0.5, **kwargs
+    ):
 
-    def __init__(self, num_layers=2, num_category=4):
+        super(SimpleLSTM, self).__init__()
 
-        super().__init__(SimpleLSTM, self)
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+
+        # embedding layer
+        self.embed = nn.Embedding(num_vocab, embed_dim)
 
         # LSTM
-        self.recurrent = nn.LSTM(input_size=128, hidden_size=32,
-                            num_layers=num_layers, batch_first=True)
-        
+        self.recurrent = nn.LSTM(
+            embed_dim, hidden_dim, num_layers=self.num_layers, batch_first=True
+        )
+
         # fully connected layers for texts
         self.fc_1 = nn.Linear(32, 16)
-        self.fc_2 = nn.Linear(16, num_category)
+        self.fc_2 = nn.Linear(16, num_classes)
 
-        self.fc_3 = nn.Linear(5, 32)
+        self.fc_3 = nn.Linear(kwargs["num_publisher"], 32)
         self.fc_4 = nn.Linear(32, 16)
-        self.fc_5 = nn.Linear(16, num_category)
+        self.fc_5 = nn.Linear(16, num_classes)
 
+        # drop-out layer
+        self.do = nn.Dropout(dropout_p)
 
-    def forward(x, z):
+    def forward(self, x, z):
         """
         Forward propagation.
 
@@ -69,37 +102,57 @@ class SimpleLSTM(nn.Module):
         - x: Batch of texts.
         - z: Batch of publisher labels.
         """
-        x = self.recurrent(x)
+
+        x = self.embed(x)
+        h_0, c_0 = self._init_state(batch_size=x.size(0))
+        x, _ = self.recurrent(x, (h_0, c_0))
+        x = x[:, -1, :]
         x = F.relu(self.fc_1(x))
         x = F.relu(self.fc_2(x))
 
-        z = F.relu(self.fc_3(z))
+        z = F.relu(self.fc_3(z.type(torch.float)))
         z = F.relu(self.fc_4(z))
         z = F.relu(self.fc_5(z))
 
-        out = F.softmax(x + z)
+        out = x + z
         return out
+
+    def _init_state(self, batch_size=1):
+        weight = next(self.parameters()).data
+        return (
+            weight.new(self.num_layers, batch_size, self.hidden_dim).zero_(),
+            weight.new(self.num_layers, batch_size, self.hidden_dim).zero_(),
+        )
 
 
 class SimpleGRU(nn.Module):
+    def __init__(
+        self, num_layers, hidden_dim, num_vocab, embed_dim, num_classes, dropout_p=0.5, **kwargs
+    ):
 
-    def __init__(self, num_layers=2, num_category=4):
+        super(SimpleGRU, self).__init__()
 
-        super().__init__(SimpleGRU, self)
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+
+        # embedding layer
+        self.embed = nn.Embedding(num_vocab, embed_dim)
 
         # LSTM
-        self.recurrent = nn.GRU(input_size=128, hidden_size=32,
-                            num_layers=num_layers, batch_first=True)
-        
+        self.recurrent = nn.GRU(embed_dim, hidden_dim, num_layers=self.num_layers, batch_first=True)
+
         # fully connected layers for texts
         self.fc_1 = nn.Linear(32, 16)
-        self.fc_2 = nn.Linear(16, num_category)
+        self.fc_2 = nn.Linear(16, num_classes)
 
-        self.fc_3 = nn.Linear(5, 32)
+        self.fc_3 = nn.Linear(kwargs["num_publisher"], 32)
         self.fc_4 = nn.Linear(32, 16)
-        self.fc_5 = nn.Linear(16, num_category)
+        self.fc_5 = nn.Linear(16, num_classes)
 
-    def forward(x, z):
+        # drop-out layer
+        self.do = nn.Dropout(dropout_p)
+
+    def forward(self, x, z):
         """
         Forward propagation.
 
@@ -107,14 +160,23 @@ class SimpleGRU(nn.Module):
         - x: Batch of texts.
         - z: Batch of publisher labels.
         """
-        x = self.recurrent(x)
+
+        x = self.embed(x)
+        h_0 = self._init_state(batch_size=x.size(0))
+        x, _ = self.recurrent(x, h_0)
+        x = x[:, -1, :]
         x = F.relu(self.fc_1(x))
         x = F.relu(self.fc_2(x))
 
-        z = F.relu(self.fc_3(z))
+        z = F.relu(self.fc_3(z.type(torch.float)))
         z = F.relu(self.fc_4(z))
         z = F.relu(self.fc_5(z))
 
-        out = F.softmax(x + z)
+        out = x + z
         return out
-        
+
+    def _init_state(self, batch_size=1):
+        weight = next(self.parameters()).data
+        return weight.new(self.num_layers, batch_size, self.hidden_dim).zero_()
+            
+
--- chapter9_rnn/train.py
+++ chapter9_rnn/train.py
@@ -1,29 +1,57 @@
+"""
+train.py: Training routine for RNN models
+"""
+
+import argparse
+from tqdm import tqdm
+
 import torch
-from torchtext.legacy import data
-
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 from torchtext.legacy.data import TabularDataset
-from torchtext.legacy.data import Iterator
-from torchtext.legacy.data.iterator import batch
-
+from torchtext.legacy.data import BucketIterator
 import utils.fields as fields
+
+from models import SimpleRNN, SimpleLSTM, SimpleGRU
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--model_type",
+    type=str,
+    default="gru",
+    help="Type of model to use. Can be one of 'rnn', 'lstm', 'gru'.",
+)
+parser.add_argument("--num_epoch", type=int, default=100, help="Number of epochs")
+parser.add_argument("--batch_size", type=int, default=10, help="Size of a batch")
+parser.add_argument("--lr", type=float, default=0.001, help="Learning rate")
+parser.add_argument("--beta1", type=float, default=0.9, help="Beta 1 of Adam optimizer")
+parser.add_argument("--beta2", type=float, default=0.999, help="Beta 2 of Adam optimizer")
+parser.add_argument("--step_size", type=int, default=100, help="Step size of StepLR")
+parser.add_argument("--gamma", type=float, default=0.99, help="Gamma of StepLR")
+args = parser.parse_args()
+
 
 def main():
 
-    # check if GPU is available
-    if torch.cuda.is_available():
-        device='gpu'
-        print('[!] CUDA device detected.')
-    else:
-        device='cpu'
-        print('[!] No available CUDA device.')
-    print('[!] Using {}'.format(device))
-    
+    # check GPU
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("[!] Using {}".format(device))
+
     # load train, valid, test data
     train_data, valid_data, test_data = TabularDataset.splits(
-        path='./data/', train='train.csv', validation='valid.csv', test='test.csv', format='csv',
-        fields=[('title', fields.TITLE), ('publisher', fields.PUBLISHER), ('category', fields.CATEGORY)],
+        path="./data/",
+        train="train.csv",
+        validation="valid.csv",
+        test="test.csv",
+        format="csv",
+        fields=[
+            ("title", fields.TITLE),
+            ("publisher", fields.PUBLISHER),
+            ("category", fields.CATEGORY),
+        ],
         skip_header=True,
-        csv_reader_params={'delimiter':','}
+        csv_reader_params={"delimiter": ","},
     )
 
     # build vocabulary
@@ -33,17 +61,30 @@ def main():
     fields.PUBLISHER.build_vocab(train_data, valid_data, test_data)
     fields.CATEGORY.build_vocab(train_data, valid_data, test_data)
 
-    # set training parameters
-    batch_size = 10
-    num_epoch = 100
+    vocab_size = len(fields.TITLE.vocab)
 
     # create data loader for each dataset
-    train_loader = Iterator(dataset=train_data, batch_size=batch_size, device=device)
-    valid_loader = Iterator(dataset=valid_data, batch_size=batch_size, device=device)
-    test_loader = Iterator(dataset=test_data, batch_size=batch_size, device=device)
+    train_loader = BucketIterator(dataset=train_data, batch_size=args.batch_size, device=device)
+    valid_loader = BucketIterator(dataset=valid_data, batch_size=args.batch_size, device=device)
+    test_loader = BucketIterator(dataset=test_data, batch_size=args.batch_size, device=device)
+
+    # create model
+    if args.model_type == "rnn":
+        model = SimpleRNN(2, 32, vocab_size, 128, 5, num_publisher=6).to(device)
+    elif args.model_type == "lstm":
+        model = SimpleLSTM(2, 32, vocab_size, 128, 5, num_publisher=6).to(device)
+    elif args.model_type == "gru":
+        model = SimpleGRU(2, 32, vocab_size, 128, 5, num_publisher=6).to(device)
+    else:
+        print("[!] Invalid configuration. Please choose one of 'rnn', 'lstm', 'gru'.")
+        return -1
+
+    # configure optimizer & scheduler
+    optimizer = optim.Adam(model.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.gamma)
 
     # iterate over epochs
-    for epoch in range(num_epoch):
+    for epoch in tqdm(range(args.num_epoch)):
 
         train_iter = iter(train_loader)
 
@@ -54,14 +95,26 @@ def main():
             except StopIteration:
                 break
 
-            title = train_batch.title
-            publisher = train_batch.publisher
-            category = train_batch.category
-            
-            """
-            some training -> prop, back prop, optimize
-            """
+            title = train_batch.title.to(device)
+            publisher = train_batch.publisher.to(device)
+            category = train_batch.category.to(device)
 
+            # one-hot encoding for categorical data
+            publisher = F.one_hot(publisher, 6)
+            category = F.one_hot(category, 5)
+
+            # forward prop
+            pred = model(title, publisher)
+
+            # calculate loss
+            loss = nn.CrossEntropyLoss()(pred, torch.argmax(category, dim=1))
+
+            # back prop
+            loss.backward()
+            optimizer.step()
+
+            # update scheduler
+            scheduler.step()
 
         with torch.no_grad():
 
@@ -72,6 +125,6 @@ def main():
             evaluate, record
             """
 
-        
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
